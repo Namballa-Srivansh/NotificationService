d2d436265798d462cb5770ea911fce66852c1ffe
Namballa-Srivansh <srivansh90@gmail.com>
2026-02-02 16:56:26 +0530

    Feat: Added ticket model

---
+++ models/ticketNotification.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const ticketNotificationSchema = new mongoose.Schema({
+    subject: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    recipientEmails: {
+        type: [String],
+        required: true
+    },
+    status: {
+        type: String,
+        enum: {
+            values: ["SUCCESS", "FAILED", "PENDING"],
+            message: "Invalid ticket status"
+        },
+        required: true
+    }
+}, {timestamps: true});
+
+const TicketNotification = mongoose.model("TicketNotification", ticketNotificationSchema);
+module.exports = TicketNotification;
