998b9919823b78fbbcb072709ffa3e7a71e68478
Namballa-Srivansh <srivansh90@gmail.com>
2026-02-19 17:14:57 +0530

    Minor Fixes

--- crons/cron.js
+++ crons/cron.js
@@ -5,7 +5,7 @@ const Mailer = require("../services/email.service");
 const mailerCron = () => {
     const mailer = Mailer(process.env.EMAIL, process.env.EMAIL_PASS);
 
-    cron.schedule('*/2 * * * *', async () => {
+    cron.schedule('*/2 * * * * *', async () => {
         console.log("Executing Cron Again")
         const notificationsToBeSent = await Ticket.find({
             status: "PENDING"
